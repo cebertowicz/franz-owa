3ee6db5cc5f8f9504158f2466d63b3c7cd4a3442
Steve Cebertowicz <s.cebertowicz@gmail.com>
2018-01-02 21:58:30 -0800

    badge count fix

--- webview.js
+++ webview.js
@@ -3,11 +3,10 @@ module.exports = (Franz) => {
     function getMessages() {
         let count = jQuery("span[title*='Inbox'] + span + span").first().text();
 
-        Franz.setBadge(count);
+        Franz.setBadge(parseInt(count));
     };
 
     // check for new messages every second and update Franz badge
     Franz.loop(getMessages);
-
 
 };
